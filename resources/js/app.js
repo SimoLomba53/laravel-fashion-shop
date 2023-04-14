83f146f2e31feaef03c91eb237f03e4761cb6ffa
MichelepioBombai <michelebombai@icloud.com>
2023-04-14 16:24:50 +0200

    add bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
-import './bootstrap';
-import '~resources/scss/app.scss';
-import * as bootstrap from 'bootstrap';
-import.meta.glob([
-    '../img/**'
-])
+import "./bootstrap";
+import "~resources/scss/app.scss";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import * as bootstrap from "bootstrap";
+import.meta.glob(["../img/**"]);
